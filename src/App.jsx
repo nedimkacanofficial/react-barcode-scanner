3bc1e949453d9b098fef20ca981d7073d7c0119e
Nedim Kaçan <nedimkacanofficial@gmail.com>
2023-09-21 00:55:58 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './App.css';
+import Html5QrcodePlugin from './Html5QrcodePlugin';
+import ResultContainerPlugin from './ResultContainerPlugin';
+
+const App = (props) => {
+    const [decodedResults, setDecodedResults] = useState([]);
+    const onNewScanResult = (decodedText, decodedResult) => {
+        console.log("App [result]", decodedResult);
+        setDecodedResults(prev => [...prev, decodedResult]);
+    };
+
+    return (
+        <div className="App">
+            <section className="App-section">
+                <div className="App-section-title"> Html5-qrcode React demo</div>
+                <br />
+                <br />
+                <br />
+                <Html5QrcodePlugin
+                    fps={10}
+                    qrbox={250}
+                    disableFlip={false}
+                    qrCodeSuccessCallback={onNewScanResult}
+                />
+                <ResultContainerPlugin results={decodedResults} />
+            </section>
+        </div>
+    );
+};
+
+export default App;
